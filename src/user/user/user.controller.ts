973b892f6885fa1bf07f7ff3b7ce7084b1b0bee2
akmalfman <akmalfsalman2@gmail.com>
2025-12-30 15:19:08 +0700

    implement http request and http response

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Post, Query, Req, } from '@nestjs/common';
-import express from 'express';
+import { Controller, Get, Header, Param, Post, Query, Req, Res, HttpCode, Redirect, } from '@nestjs/common';
+import type { HttpRedirectResponse } from '@nestjs/common';
+import type { Response, Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
@@ -15,17 +16,35 @@ export class UserController {
     }
 
     // @Get('/:id')
-    // getById(@Req() request: express.Request): string {
+    // getById(@Req() request: Request): string {
     //     return `GET ${request.params.id}`
     // }
 
-    @Get('/:id')
-    getById(@Param("id") id: string): string {
-        return `GET ${id}`
+    // @Get('/:id')
+    // getById(@Param("id") id: string): string {
+    //     return `GET ${id}`
+    // }
+
+    // @Get("/sample")
+    // get(): string {
+    //     return "Get";
+    // }
+
+    @Get("/sample-response")
+    @Header('Content-Type', 'application/json')
+    @HttpCode(200)
+    sampleResponse(): Record<string, string> {
+        return ({
+            data: "Hell"
+        });
     }
 
-    @Get("/sample")
-    get(): string {
-        return "Get";
+    @Get('/redirect')
+    @Redirect()
+    redirect(): HttpRedirectResponse {
+        return {
+            url: '/api/users/sample-response',
+            statusCode: 301
+        };
     }
 }
